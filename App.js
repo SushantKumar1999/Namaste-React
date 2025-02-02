62cde55d5c706d9ad227d7ded397c584521cb473
Sushant Kumar <sushantkumar19990819@gmail.com>
2025-02-02 23:11:45 +0530

    Component Composition

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React Element
+const element = <span>React Element</span>
 
+
+
+// React Element 
+const titles = (
+  <h1> 
+    this is an element 
+   {element}     {/*// this is how we put react element inside a react element */}
+  </h1>
+)
+
+// React Functional Component 
 const Title = () => (
-  <h1> Sushant Kumar</h1>
+  <h1> This is a functional Component </h1>
 )
 
 // React Functional Component
 const HeadingComponent = () => (
   <div id="container">
-    <Title />
+     <Title />   {/*this is how we put a component inside a component  */}
+    {titles}    {/*this is how we put element inside a component  */}
     <h1>this is HeadingComponent </h1>    
   </div>
 )
 
-// What we have done above is the component composition.
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
